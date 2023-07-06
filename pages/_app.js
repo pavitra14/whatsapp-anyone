486f3094dcee7df29601ddb7cd8fd8e28cfc68c5
Pavitra Behre <behrepav@amazon.com>
2023-07-06 16:48:05 +0530

    Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/global.css';
 import 'react-intl-tel-input/dist/main.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Component {...pageProps} />
+  <Analytics />
+  </>
 }
